ea0db851c3524d2016f6ea5da50efc35868ab4d6
uinox <326138915@qq.com>
2024-05-29 17:16:59 +0800

    check-file

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
         'handle-event-naming': require('./rules/handle-event-naming'),
         'css-naming': require('./rules/css-naming'),
         'constant-naming': require('./rules/constant-naming'),
+        'check-file': require('./rules/check-file'),
     }
 };
---
+++ rules/check-file.js
@@ -0,0 +1,108 @@
+const casing = require("../utils/casing");
+const allowedCaseOptions = ['camelCase', 'snake_case', 'PASCAL_CASE'];
+
+module.exports = {
+    meta: {
+        type: 'suggestion',
+        docs: {
+            description: 'Ensure file name matching caseTypes',
+            recommended: true
+        },
+        fixable: 'code',
+        schema: [
+            {
+                type: "object",
+                properties: {
+                    caseTypes: {
+                        type: "array",
+                        items: {
+                            type: "string",
+                            enum: allowedCaseOptions
+                        },
+                        uniqueItems: true,
+                        additionalItems: false
+                    },
+                    fileTypes: {
+                        type: "array",
+                        items: {
+                            type: "string"
+                        },
+                        uniqueItems: true,
+                        additionalItems: false
+                    },
+                    ignores: {
+                        type: "array",
+                        items: {
+                            type: "string"
+                        },
+                        uniqueItems: true,
+                        additionalItems: false
+                    },
+                },
+                additionalProperties: false
+            }
+        ],
+        messages: {
+            avoidFileName: "File '{{ name }}' is not in ['{{ types }}'] case."
+        }
+    },
+    create(context){
+        return {
+            Program(node){
+                const { fileTypes } = context.options[0];
+                const fileName = context.getFilename().match(/\w+.\w+$/)[0];
+                const componentName = fileName.match(/\w+/)[0];
+
+                // whether it belongs to fileTypes
+                let isFileTypes = false;
+                if(fileTypes && fileTypes.length > 0){
+                    fileTypes.forEach(fileType=>{
+                        if(fileName.name.endsWith(fileType)){
+                            isFileTypes = true;
+                        }
+                    })
+                }
+                
+                // whether it belongs to ignored
+                let isIgnore = false;
+                if(ignores && ignores.length > 0){
+                    ignores.forEach(ignore=>{
+                        if(componentName.includes(ignore)){
+                            isIgnore = true;
+                        }
+                    })
+                }
+                
+                // whether it needs to be checked
+                let needCheck = false;
+                if(!isIgnore || isFileTypes){
+                    needCheck = true;
+                }
+
+                if(needCheck){
+                    // check caseType
+                    let isCaseChecker = false;
+                    if(caseTypes && caseTypes.length > 0){
+                        caseTypes.forEach(caseType => {
+                            const caseChecker = casing.getChecker(caseType);
+                            if(caseChecker(componentName)){
+                                isCaseChecker = true;
+                            }
+                        })
+                    }
+
+                    if(!isCaseChecker){
+                        context.report({
+                            node,
+                            messageId: "avoidFileName",
+                            data: {
+                                name: componentName,
+                                types: caseTypes.join(', ')
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
